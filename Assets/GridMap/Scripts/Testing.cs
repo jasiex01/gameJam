2225fc18a1474277f32d0439939d69041f1d585c
jasiex01 <hernasj@wp.pl>
2022-12-10 00:14:37 +0100

    started adding grid

---
+++ Assets/GridMap/Scripts/Testing.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        Grid grid = new Grid(10, 8, 1f);
+    }
+
+}
