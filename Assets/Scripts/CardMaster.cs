db8a3e450063e6e702a179f7833ad17b512fafd8
jasiex01 <hernasj@wp.pl>
2022-12-10 15:18:15 +0100

    code clarity improvements

--- Assets/Scripts/CardMaster.cs
+++ Assets/Scripts/CardMaster.cs
@@ -12,57 +12,55 @@ public class CardMaster : MonoBehaviour
     public RectTransform anchor;
     public UICard cardPrefab;
     private List<UICard> cardList = new ();
-    public int cardCount;
+    public int initialCardCount;
     public float spacing;
     public DOTweenAnimationTemplate cardMoveAnimation;
     public DOTweenAnimationTemplate cardScaleAnimation;
-    private UICard chosenCard = null;
-    public GridCursor cursor;
+    public GameMaster gameMaster;
 
     // Start is called before the first frame update
     void Start()
     {
-        ResetCards();
     }
 
-    private void ResetCards() {
-        chosenCard = null;
-        cursor.ChangeVisibility(false);
+    public void ResetCards() {
+        
         for (int i = 0; i < cardList.Count; i++){
             Destroy(cardList[i].gameObject);
         }
 
         cardList.Clear();
-
-        for (int i = 0; i < cardCount; i++){
+        // Instantiate cards
+        for (int i = 0; i < initialCardCount; i++){
             var newCard = Instantiate(cardPrefab, anchor);
             newCard.cardMaster = this;
             newCard.transform.localPosition = new Vector3(0,-300,0);
             cardList.Add(newCard);
         }
-        // Instantiate cards
-        float offset = (-1 * cardCount * spacing) / 2;
-        for (int i = 0; i < cardCount; i++){
+        
+        UpdateCards();
+    }
+
+    private void UpdateCards(){
+        float offset = (-1 * (cardList.Count - 1) * spacing) / 2;
+        for (int i = 0; i < cardList.Count; i++){
             var position = new Vector3(offset + (i * spacing), 0, 0);
             cardList[i].transform.DOLocalMove(position, cardMoveAnimation.duration).SetEase(cardMoveAnimation.easeType);
         }
-
     }
+
 
     // Update is called once per frame
     void Update()
-    {
-        if(Keyboard.current.spaceKey.wasPressedThisFrame){
-            ResetCards();
-        }
-        
+    {        
     }
 
     public void OnCardClicked(UICard card){
         card.transform.DOScale(new Vector3(1.2f,1.2f,1.2f), cardScaleAnimation.duration);
-        chosenCard = card;
-        cursor.ChangeVisibility(true);
-        for (int i = 0; i < cardCount; i++){
+
+        gameMaster.onCardClicked(card);
+        
+        for (int i = 0; i < initialCardCount; i++){
             if(cardList[i] != card)
                 cardList[i].transform.DOScale(new Vector3(1,1,1), cardScaleAnimation.duration);
         }
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameMaster : MonoBehaviour
+{
+    public CardMaster cardMaster;
+    public GridCursor cursor;
+    private UICard chosenCard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame){
+            ResetCards();
+        }
+    }
+    public void onCardClicked(UICard card){
+        chosenCard = card;
+        cursor.ChangeVisibility(true);
+    }
+
+    private void ResetCards(){
+        chosenCard = null;
+        cursor.ChangeVisibility(false);
+        cardMaster.ResetCards();
+    }
+}
