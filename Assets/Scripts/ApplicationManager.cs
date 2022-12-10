e837bc262bd2d117f27a5931075c426e9c136c4a
Dominik <74020131+Domciotomcio@users.noreply.github.com>
2022-12-10 16:22:30 +0100

    Working intros

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ApplicationManager : MonoBehaviour {
 	
-	public void Start()
+	public void StartGame()
 	{
-		Debug.Log("HELLO");
+		Debug.Log("Go to intro");
+		SceneManager.LoadScene("Intro1");  
 	}
 
 	public void Tutorial()
 	{
 		Debug.Log("Tutorial");
 	}
+
+	public void Scene3() {  
+        SceneManager.LoadScene("Scene3");  
+    } 
 
 	public void Quit () 
 	{
